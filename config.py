0f6ba63adffcfe0da711d96c9d5d90225622e207
Cassie Huang <35473217+cassieHuanghahaha@users.noreply.github.com>
2019-05-19 16:22:34 -0700

    update config.py

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ DEBUG = True
 
 #MONGODB_DB = 'partygo'
 MONGODB_SETTINGS = {
-   'host':'mongodb://partygoAdmin:b4wgWkP123@partygo-shard-00-00-zi3c5.gcp.mongodb.net:27017,partygo-shard-00-01-zi3c5.gcp.mongodb.net:27017,partygo-shard-00-02-zi3c5.gcp.mongodb.net:27017/partygo?ssl=true&replicaSet=partygo-shard-0&authSource=admin&retryWrites=true'
-
+   'host':''
 PROJECT_ID = ''
 CLOUD_STORAGE_BUCKET = ''
