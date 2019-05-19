e2f4d987dd3f0bb2d88d441acdc1497cc2884218
cassie.huang1111 <cassie.huang1111@gmail.com>
2019-05-19 19:09:41 -0400

    first add

---
+++ config.py
@@ -0,0 +1,10 @@
+SECRET_KEY = 'zhisheng'
+DEBUG = True
+
+#MONGODB_DB = 'partygo'
+MONGODB_SETTINGS = {
+   'host':'mongodb://partygoAdmin:b4wgWkP123@partygo-shard-00-00-zi3c5.gcp.mongodb.net:27017,partygo-shard-00-01-zi3c5.gcp.mongodb.net:27017,partygo-shard-00-02-zi3c5.gcp.mongodb.net:27017/partygo?ssl=true&replicaSet=partygo-shard-0&authSource=admin&retryWrites=true'
+#     'host' : 'mongodb+srv://partygo2:b4wgWkP123@cluster0-zi3c5.gcp.mongodb.net/cluster0?retryWrites=true'
+}
+PROJECT_ID = 'partygo-241022'
+CLOUD_STORAGE_BUCKET = 'partygogo-bucket'
